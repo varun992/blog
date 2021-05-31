f01d0a0db36302d8fa8360f5e7642fb41500af28
Vishnani <vvishnan@amazon.com>
2021-05-31 12:26:11 +0530

    use django python local settings for heroku

--- RocketLearn/settings.py
+++ RocketLearn/settings.py
@@ -18,8 +18,8 @@ env = Env()
 env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -148,6 +148,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 #Email setup
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 CSRF_COOKIE_SECURE = True
